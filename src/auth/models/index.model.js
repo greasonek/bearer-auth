a0ba11b8c7502e99f6ec5d739382929690172f71
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-10-21 14:18:07 -0700

    took out max age from user.js

--- src/auth/models/index.model.js
+++ src/auth/models/index.model.js
@@ -3,21 +3,20 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users.js');
+const todo = require('./todo.js');
+const Collection = require('./collection');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL;
 //const users = userSchema(sequelize, Datatypes);
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
-  dialectOptions: {
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false,
-    // }
-  }
-} : {};
+  dialectOptions: {} } : {};
 
 const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+const todoModel = todo(sequelize, DataTypes);
+const todoCollection = new Collection(todoModel);
 
 module.exports = {
   db: sequelize,
   users: userSchema(sequelize, DataTypes),
+  todoCollection,
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const acl = require('./auth/middleware/acl');
+const bearer = require('./auth/middleware/bearer');
+
+
 // 3rd Party Resources
 const express = require('express');
 const cors = require('cors');
@@ -9,6 +13,7 @@ const morgan = require('morgan');
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/router/index.router.js');
+const todoRoutes = require('./auth/router/todo.router');
 
 // Prepare the express app
 const app = express();
@@ -22,10 +27,26 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use(authRoutes);
+app.use(todoRoutes);
 
 // Catchalls
 app.use(notFound);
 app.use(errorHandler);
+
+
+// add routes that are based on permission based roles
+app.get('/read', bearer, acl('read'), (req, res) => {
+  res.status(200).send('you have read access');
+});
+app.post('/create', bearer, acl('c`reate'), (req, res) => {
+  res.status(200).send('you have create access');
+});
+app.put('/update', bearer, acl('update'), (req, res) => {
+  res.status(200).send('you have update access');
+});
+app.delete('/delete', bearer, acl('delete'), (req, res) => {
+  res.status(200).send('you have delete access');
+});
 
 module.exports = {
   server: app,
